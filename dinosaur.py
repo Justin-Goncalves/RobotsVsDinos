044cce0d71c81ea75e0f90aa99a438081281390e
Justin Goncalves <jvgoncalves85@gmail.com>
2022-03-25 22:00:58 -0400

    Debugged Dinosaur class and tested it as well

--- dinosaur.py
+++ dinosaur.py
@@ -1,10 +1,15 @@
+
+
 class Dinosaur:
-    def __init__(self, dino_name):
+    def __init__(self, dino_name,attack_power):
         self.dino_name = dino_name
         self.dino_health = 100
-        self.dino_attack_power = 10
-
+        self.dino_attack_power = attack_power
+# we don't need to further notate for the dino_attack_power
+# there is no further value
+# dino_attack_power is at the end
     def dino_attack(self, robot):
         robot.robot_health -= self.dino_attack_power
-        
+        print(f"{robot.robot_name} has been attacked for a damage of {self.dino_attack_power}")
+        print(f"{robot.robot_name} health is at {robot.robot_health}")
 
--- robot.py
+++ robot.py
@@ -6,7 +6,8 @@ class Robot:
         self.robot_name = robot_name
         self.robot_health = 100
         self.robot_weapon = Weapon("Sword", 30)
-
+# We had to further dot notate for robot weapon because in weapon we called a class. 
+# Need to further notate to let python know where the attack_power is in that class
     def robo_attack(self, dinosaur):
         dinosaur.dino_health -= self.robot_weapon.attack_power
         print(f"{dinosaur.dino_name} has been attacked for a damage of {self.robot_weapon.attack_power}")

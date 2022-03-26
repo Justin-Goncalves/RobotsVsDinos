79cfb7002573f4f4d1534c8a974fdb384fcc8a8b
Justin Goncalves <jvgoncalves85@gmail.com>
2022-03-25 21:42:08 -0400

    Created weapon class and tested Robot class

--- robot.py
+++ robot.py
@@ -1,13 +1,13 @@
 from weapon import Weapon
 
 
-
 class Robot:
     def __init__(self, robot_name):
         self.robot_name = robot_name
         self.robot_health = 100
-        self.robot_weapon = Weapon()
+        self.robot_weapon = Weapon("Sword", 30)
 
     def robo_attack(self, dinosaur):
-        dinosaur.dino_health -= self.robot_weapon
+        dinosaur.dino_health -= self.robot_weapon.attack_power
+        print(f"{dinosaur.dino_name} has been attacked for a damage of {self.robot_weapon.attack_power}")
 
